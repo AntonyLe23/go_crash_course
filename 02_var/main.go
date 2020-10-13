5de9231f70772bfcdb0b87b2b871956b848d2e18
Le Thinh Phat <phatlt01@acb.com.vn>
2020-10-13 09:43:06 +0700

    commit bài 2 và 3

--- 02_var/main.go
+++ 02_var/main.go
@@ -9,11 +9,11 @@ func main() {
 	const isCool = true
 
 	//Shorthand
-	name := "Phat"
+	name, email := "Phat", "phat@mail.com"
 	size := 1.3
 	var otherSize float32 = 2.3
 
-	fmt.Println(name, age, isCool)
+	fmt.Println(name, age, isCool, email)
 	fmt.Printf("%T\n", size)
 	fmt.Printf("%T\n", otherSize)
 }
