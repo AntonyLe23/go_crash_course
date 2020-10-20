a5af2de181927ca449410dcf6df2be2854a163e1
Le Thinh Phat <phatlt01@acb.com.vn>
2020-10-20 13:43:42 +0700

    additional

---
+++ 16_channels/main.go
@@ -0,0 +1,42 @@
+/*
+
+Channels
+
+Channels are a typed conduit through which you can send and receive values with the channel operator, <-.
+
+ch <- v    // Send v to channel ch.
+v := <-ch  // Receive from ch, and assign value to v.
+
+(The data flows in the direction of the arrow.)
+
+Like maps and slices, channels must be created before use:
+
+ch := make(chan int)
+
+By default, sends and receives block until the other side is ready.
+This allows goroutines to synchronize without explicit locks or condition variables.
+
+The example code sums the numbers in a slice, distributing the work between two goroutines.
+Once both goroutines have completed their computation, it calculates the final result.
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{1, 4, 1, 5, 2, 6}
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c //recieve from c
+	fmt.Println(x, y, x+y)
+}
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to channel c
+}
